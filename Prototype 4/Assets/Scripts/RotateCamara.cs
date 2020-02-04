c9a3fdae9b6070e6b42c2600220f380fd24e5572
360-Lui <lm69234@srcs.org>
2020-02-04 14:52:50 -0800

    Lesson 4.1.6 completed

--- Prototype 4/Assets/Scripts/RotateCamara.cs
+++ Prototype 4/Assets/Scripts/RotateCamara.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamara : MonoBehaviour
 {
-    public float rotationSpeed;
+    [SerializeField] private float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
