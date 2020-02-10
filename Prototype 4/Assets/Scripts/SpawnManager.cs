bc4f5d3a44ae387129f50f6412a3592c8cd03bb5
360-Lui <lm69234@srcs.org>
2020-02-10 14:56:20 -0800

    Lesson 4.4.4 completed

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,7 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     private float spawnRange = 9.0f;
+    [SerializeField] private int enemyCount;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
